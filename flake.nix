05ce45f695f95753a45b20c4865b83afd283750c
Wojtek Czekalski <me@wczekalski.com>
2025-09-23 10:48:23 +0200

    Migrate to uv

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "ZERO VAD";
+  nixConfig = {
+    extra-substituters = ["https://nix-community.cachix.org"];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-gl-host.url = "github:numtide/nix-gl-host";
+    nix-gl-host.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    nix-gl-host,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        config.cudaSupport = true;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs;
+          [
+            python311
+            uv
+            alejandra
+            protobuf
+            portaudio
+          ]
+          ++ (
+            if pkgs.stdenv.isLinux
+            then [
+              nix-gl-host.defaultPackage.${system}
+            ]
+            else []
+          );
+        "LD_LIBRARY_PATH" = pkgs.lib.makeLibraryPath (with pkgs;
+          [
+          ]
+          ++ (
+            if pkgs.stdenv.isLinux
+            then [
+              cudaPackages_12_1.backendStdenv.cc.cc.lib
+            ]
+            else []
+          ));
+        shellHook =
+          if pkgs.stdenv.isLinux
+          then ''
+            export LD_LIBRARY_PATH=$(${nix-gl-host.defaultPackage.${system}}/bin/nixglhost -p):$LD_LIBRARY_PATH
+            # Set LIBRARY_PATH to help the linker find the CUDA static libraries
+            export LIBRARY_PATH=${
+              pkgs.lib.makeLibraryPath [
+                pkgs.portaudio
+              ]
+            }:$LIBRARY_PATH
+          ''
+          else ''
+            export DYLD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [pkgs.portaudio]}:$DYLD_LIBRARY_PATH"
+          '';
+      };
+    });
+}
+
